63d077f4924c131e2063f47ad13205bc54dffd85
Andy Lu <alu@futuredial.com>
2021-01-08 17:20:24 +0800

    refactoring: extract method from when-statement

--- src/main/kotlin/PriceCalculator.kt
+++ src/main/kotlin/PriceCalculator.kt
@@ -1,43 +1,32 @@
 class PriceCalculator {
     fun calculatePrice(store: String, unitPrice: Int, amount: Int): Number {
         return when (store) {
-            "7-11" -> {
-                getPriceBy711(unitPrice, amount)
-            }
-            "carrefour" -> {
-                getPriceByCarrefour(amount, unitPrice)
-            }
-            "pchome" -> {
-                getPriceByPchome(unitPrice, amount)
-            }
-
-            "watsons" -> {
-                getPriceByWatsons(unitPrice, amount)
-            }
+            "7-11" -> getPriceBy711(unitPrice, amount)
+            "carrefour" -> getPriceByCarrefour(amount, unitPrice)
+            "pchome" -> getPriceByPchome(unitPrice, amount)
+            "watsons" -> getPriceByWatsons(unitPrice, amount)
             else -> 0
         }
     }
 
-    private fun getPriceByWatsons(unitPrice: Int, amount: Int) =
-        if (getPrice(unitPrice, amount) * 0.88 >= 1000) {
-            getPrice(unitPrice, amount) * 0.88 * 0.8
-        } else {
-            getPrice(unitPrice, amount) * 0.88
-        }
-
-    private fun getPriceByPchome(unitPrice: Int, amount: Int) = if (getPrice(unitPrice, amount) >= 1000) {
-        getPrice(unitPrice, amount) - 100
+    private fun getPriceByWatsons(unitPrice: Int, amount: Int) = if (unitPrice * amount * 0.88 >= 1000) {
+        unitPrice * amount * 0.88 * 0.8
     } else {
-        getPrice(unitPrice, amount)
+        unitPrice * amount * 0.88
+    }
+
+    private fun getPriceByPchome(unitPrice: Int, amount: Int) = if (unitPrice * amount >= 1000) {
+        unitPrice * amount - 100
+    } else {
+        unitPrice * amount
     }
 
     private fun getPriceByCarrefour(amount: Int, unitPrice: Int) = if (amount <= 100) {
-        getPrice(unitPrice, amount) * 0.85
+        unitPrice * amount * 0.85
     } else {
-        getPrice(unitPrice, amount) * 0.8
+        unitPrice * amount * 0.8
     }
 
-    private fun getPriceBy711(unitPrice: Int, amount: Int) = getPrice(unitPrice, amount)
+    private fun getPriceBy711(unitPrice: Int, amount: Int) = unitPrice * amount
 
-    private fun getPrice(unitPrice: Int, amount: Int) = unitPrice * amount
 }
