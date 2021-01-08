29a3cbd5caa5a496ee682ef67ac7be5ce376d109
Andy Lu <alu@futuredial.com>
2021-01-08 17:47:59 +0800

    refactoring: use StoreStrategy instead each concrete store class

--- src/main/kotlin/PriceCalculator.kt
+++ src/main/kotlin/PriceCalculator.kt
@@ -1,10 +1,25 @@
 class PriceCalculator {
+
     fun calculatePrice(store: String, unitPrice: Int, amount: Int): Number {
+        val storeStrategy: StoreStrategy
+
         return when (store) {
-            "7-11" -> SevenEleven().price(unitPrice, amount)
-            "carrefour" -> Carrefour().price(amount, unitPrice)
-            "pchome" -> PcHome().price(unitPrice, amount)
-            "watsons" -> Watsons().price(unitPrice, amount)
+            "7-11" -> {
+                storeStrategy = SevenEleven()
+                storeStrategy.price(unitPrice, amount)
+            }
+            "carrefour" -> {
+                storeStrategy = Carrefour()
+                storeStrategy.price(unitPrice, amount)
+            }
+            "pchome" -> {
+                storeStrategy  = PcHome()
+                storeStrategy.price(unitPrice, amount)
+            }
+            "watsons" -> {
+                storeStrategy = Watsons()
+                storeStrategy.price(unitPrice, amount)
+            }
             else -> 0
         }
     }
